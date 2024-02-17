0b45f5a35c96f6b58092512694e228068e63c13f
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-17 16:51:40 +0600

    with map and array listWhellscrooll

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -25,7 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
- 
   final String title;
 
   @override
@@ -33,58 +31,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- 
-
- 
-
   @override
   Widget build(BuildContext context) {
-    
+    var arrayindex = [1,2,3,4,5,6,7,8,10,11];
+
     return Scaffold(
       appBar: AppBar(
-       
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-     
         title: Text(widget.title),
       ),
       body: Center(
         child: ListWheelScrollView(
           itemExtent: 100,
-          children: [
-            Padding(
+          children:arrayindex.map((value) => Center(
+            child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                width:double.infinity,
-                color: Colors.amber,
+                width: double.infinity,
+                color: Colors.blue,
+                
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width:double.infinity,
-                color: Colors.amber,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width:double.infinity,
-                color: Colors.amber,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width:double.infinity,
-                color: Colors.amber,
-              ),
-            ),
-          ],
-
+          )).toList(),
+          
         ),
       ),
-      
-     
     );
   }
 }
